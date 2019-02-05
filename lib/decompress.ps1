e034e95b4a931d300d53f0f0dee9dbddfe2d0d34
Kied T L <eli.georgios@gmail.com>
2019-02-05 14:07:40 -0500

    Add decompress.ps1

---
+++ lib/decompress.ps1
@@ -0,0 +1,31 @@
+function requires_7zip($manifest, $architecture) {
+    foreach($dlurl in @(url $manifest $architecture)) {
+        if(file_requires_7zip $dlurl) { return $true }
+    }
+}
+
+function requires_lessmsi ($manifest, $architecture) {
+    $useLessMsi = get_config MSIEXTRACT_USE_LESSMSI
+    if (!$useLessMsi) { return $false }
+
+    $(url $manifest $architecture | Where-Object {
+        $_ -match '\.(msi)$'
+    } | Measure-Object | Select-Object -exp count) -gt 0
+}
+
+function file_requires_7zip($fname) {
+    $fname -match '\.((gz)|(tar)|(tgz)|(lzma)|(bz)|(bz2)|(7z)|(rar)|(iso)|(xz)|(lzh)|(nupkg))$'
+}
+
+function extract_7zip($path, $to, $recurse) {
+    $output = 7z x "$path" -o"$to" -y
+    if($lastexitcode -ne 0) { abort "Exit code was $lastexitcode." }
+
+    # check for tar
+    $tar = (split-path $path -leaf) -replace '\.[^\.]*$', ''
+    if($tar -match '\.tar$') {
+        if(test-path "$to\$tar") { extract_7zip "$to\$tar" $to $true }
+    }
+
+    if($recurse) { Remove-Item $path } # clean up intermediate files
+}
