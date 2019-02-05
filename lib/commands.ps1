8fb5b77f21272d94bd70d4d0b7593fa00f57cd76
Kied T L <eli.georgios@gmail.com>
2019-02-05 14:07:38 -0500

    Add commands.ps1

---
+++ lib/commands.ps1
@@ -0,0 +1,24 @@
+function command_files {
+    (Get-ChildItem (relpath '..\libexec')) | Where-Object { $_.name -match 'kombucha-.*?\.ps1$' }
+}
+
+function commands {
+    command_files | ForEach-Object { command_name $_ }
+}
+
+function command_name($filename) {
+    $filename.name | Select-String 'kombucha-(.*?)\.ps1$' | ForEach-Object { $_.matches[0].groups[1].value }
+}
+
+function command_path($cmd) {
+    $cmd_path = relpath "..\libexec\kombucha-$cmd.ps1"
+
+    $cmd_path
+}
+
+function exec($cmd, $arguments) {
+    $cmd_path = command_path $cmd
+
+    & $cmd_path @arguments
+}
+
