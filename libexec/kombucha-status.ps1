cd15870fa9ce3fff3cf730f0f579028b60a378ba
Kied T L <eli.georgios@gmail.com>
2019-02-06 08:49:38 -0500

    Add prototype of command implementation

--- libexec/kombucha-status.ps1
+++ libexec/kombucha-status.ps1
@@ -5,3 +5,22 @@
 # Options:
 #     -l, --latest       Get the latest version of the package (may take longer)
 
+. "$psscriptroot\..\lib\core.ps1"
+. "$psscriptroot\..\lib\getopt.ps1"
+
+$opt, $extra_args, $err = getopt $args 'l' 'latest'
+$latest = $opt.latest -or $opt.l
+$E = [char]27
+
+trap { }
+
+push-location
+
+if (!(test-path '.\kombucha.json')) {
+	error "440 Required file 'kombucha.json' not found."
+	error "Make sure you are in the correct project directory, or run 'kombucha init'."
+	break
+}
+
+
+pop-location
