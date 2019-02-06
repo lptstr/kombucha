ed9795ce168878351e0e4cae0ad8771e4cfc55d7
Kied T L <eli.georgios@gmail.com>
2019-02-06 08:49:07 -0500

    Add help message

---
+++ libexec/kombucha-status.ps1
@@ -0,0 +1,7 @@
+# Usage: kombucha status [options]
+# Summary: Get the status of the current project.
+# Help: Get a list of packages installed, with the current and lastest versions
+#
+# Options:
+#     -l, --latest       Get the latest version of the package (may take longer)
+
