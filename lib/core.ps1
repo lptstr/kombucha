4cd662933e56a0c940cceb37fa12f24bafb4f862
Kied T L <eli.georgios@gmail.com>
2019-02-14 09:02:22 -0500

    Add new restructuring changes to core

--- lib/core.ps1
+++ lib/core.ps1
@@ -2,12 +2,6 @@ function getversion() {
 	"1.0.0"
 }
 
-# Test the internst connection
-function test_internet() {
-	$conn = (Test-Connection -ComputerName google.com -Count 2 -Quiet)
-	return $conn
-}
-
 function is_installed($pkg) {
 	$isin = $false
 	$packages = @()	
@@ -39,13 +33,6 @@ function update_manifest($pkg, $ver) {
 	($manifest.packages[$pos]).version = $ver
 	set-content './kombucha.json' ($manifest | convertto-json)
 }
-
-# Get's the User Agent, if your program downloads something from the internet.
-function Get-UserAgent() {
-	$version = getversion
-	return ("Kombucha/v$version (+http://github.com/lptstr/kombucha/) PowerShell/$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor) (Windows NT $([System.Environment]::OSVersion.Version.Major).$([System.Environment]::OSVersion.Version.Minor); $(if($env:PROCESSOR_ARCHITECTURE -eq 'AMD64'){'Win64; x64; '})$(if($env:PROCESSOR_ARCHITEW6432 -eq 'AMD64'){'WOW64; '})$PSEdition)")
-}
-
 
 function abort($msg, [int] $exit_code=1) { write-host $msg -f red; exit $exit_code }
 function error($msg) { write-host "ERROR $msg" -f darkred }
@@ -82,57 +69,4 @@ function debug($obj) {
 }
 function success($msg) { write-host $msg -f darkgreen }
 
-function filesize($length) {
-    $gb = [math]::pow(2, 30)
-    $mb = [math]::pow(2, 20)
-    $kb = [math]::pow(2, 10)
-
-    if($length -gt $gb) {
-        "{0:n1} GB" -f ($length / $gb)
-    } elseif($length -gt $mb) {
-        "{0:n1} MB" -f ($length / $mb)
-    } elseif($length -gt $kb) {
-        "{0:n1} KB" -f ($length / $kb)
-    } else {
-        "$($length) B"
-    }
-}
-
-
-function fname($path) { split-path $path -leaf }
-function url_fname($url) { split-path (new-object uri $url).absolutePath -leaf }
-function fname($path) { split-path $path -leaf }
-function strip_ext($fname) { $fname -replace '\.[^\.]*$', '' }
-function strip_filename($path) { $path -replace [regex]::escape((fname $path)) }
-function strip_fragment($url) { $url -replace (new-object uri $url).fragment }
-
-function is_directory([String] $path) {
-	return (Test-Path $path) -and (Get-Item $path) -is [System.IO.DirectoryInfo]
-}
-
-function dl_file($src, $dest) {
-	$wc = New-Object Net.Webclient
-	$wc.Headers.add('Referer', (strip_filename $src))
-	$wc.Headers.Add('User-Agent', (Get-UserAgent))
-	$wc.downloadFile($url, $dest)
-}
-function dl_string($src) {
-	$wc = New-Object Net.Webclient
-	$wc.headers.add('Referer', (strip_filename $src))
-	$wc.Headers.Add('User-Agent', (Get-UserAgent))
-	return $wc.downloadString($src)
-}
-
-# The following functions convert Windows paths friendly paths and back
-# Example,
-# C:\Users\misspiggy\Documents\Work\mydocument.docx to ~\documents\work\mydocument.docx
-function friendly_path($path) {
-	$h = (Get-PsProvider 'FileSystem').home
-	if(!$h.endswith('\')) { $h += '\' }
-	if($h -eq '\') { return $path }
-	return ("$path" -replace ([regex]::escape($h)), "~\").ToLower()
-}
-function unfriendly_path($path) { return "$path" -replace "~", "$((Get-PsProvider 'FileSystem').home)" }
-
-function relpath($path) { "$($myinvocation.psscriptroot)\$path" } # relative to calling script
 
